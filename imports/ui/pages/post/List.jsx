f00e0dee657112527897eb271269218969f82c98
cotycarul <cotycarul@gmail.com>
2017-07-21 12:46:34 +0300

    cleaned some bugs and added logout button

--- imports/ui/pages/post/List.jsx
+++ imports/ui/pages/post/List.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import {Meteor} from 'meteor/meteor'
-import PostCreate from 'imports/ui/pages/post/PostCreate.jsx'
+
+
 export default class List extends React.Component {
     componentWillMount() {
         if(!Meteor.userId()) {
             alert("You are not logged in, you will be redirected to the login page")
-            FlowRouter.go('/');
+            //FlowRouter.go('/');
+            FlowRouter.redirect('/');
+
         }
+    }
+    logout() {
+        Meteor.logout();
+        FlowRouter.redirect('/');
     }
     render() {
         return (
             <div>
-
+                <button className="glyphicon-object-align-right" onClick={this.logout.bind(this)}>Log out</button>
             </div>
         )
     }
